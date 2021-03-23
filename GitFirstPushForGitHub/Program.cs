841a9dd452c79d5b30604b82201895f950a1c8f3
Tamu0021 <czr29479.gmail.com>
2021-03-24 07:05:13 +0900

    first commit

---
+++ GitFirstPushForGitHub/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GitFirstPushForGitHub
+{
+    class Program
+    {
+        public string workingDirectory;
+        static void Main(string[] args)
+        {
+            /* githubに上げたい作業用フォルダの入力 */
+            Console.WriteLine("Please write your git directory");
+            var gitDirectoryPath = Console.ReadLine();
+
+            /* githubのリポジトリの入力 */
+            Console.WriteLine("Please write your github repository");
+            var githubRepositoryPath = Console.ReadLine();
+
+            /* コミット時のコメント */
+            var comment = "\"first commit\"";
+
+            /* githubに上げたいフォルダを引数として、クラスのインスタンス化を行う。 */
+            UpdateForGit updateForGit = new UpdateForGit(gitDirectoryPath);
+            /* フォルダ内のgit環境を整える。 */
+            updateForGit.gitCommand($@"init");
+            updateForGit.gitCommand($@"remote add origin {githubRepositoryPath}");
+            updateForGit.gitCommand($@"add *");
+            /* ローカルコミット */
+            updateForGit.gitCommand($@"commit -m {comment}");
+            /* githubのリモートにプッシュする。 */
+            updateForGit.gitCommand($@"push origin master");
+        }
+
+    }
+    class UpdateForGit
+    {
+        private string workingDirectory;
+        public UpdateForGit(string workingDirectory)
+        {
+            this.workingDirectory = workingDirectory;
+        }
+        private int executeCommand(string command, string arguments = "")
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(command)
+                {
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    WorkingDirectory = this.workingDirectory,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+        public void gitCommand(string arguments)
+        {
+            if (executeCommand("git", arguments) != 0)
+            {
+                throw new Exception("git command error");
+            }
+        }
+    }
+}
